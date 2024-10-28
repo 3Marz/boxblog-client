d774d94e78b36c2efb71485fe7253da1dce8f47e
3Marz <omar490v@gmail.com>
2024-10-28 03:02:43 +0300

    added detecting a user that have been signup

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import Signup from "./pages/Signup";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import CategorizedBlogs from "./pages/CategorizedBlogs";
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const Layout = () => {
 	return (
@@ -56,17 +58,43 @@ const router = createBrowserRouter([
 	}
 ]);
 
+type UserContextType = {
+	userId: number | null
+	setUserId: React.Dispatch<React.SetStateAction<UserContextType['userId']>>
+}
+export const UserContext = createContext<UserContextType | null>(null)
+
 function App() {
 
+	const [userId, setUserId] = useState<number | null>(null)
+
+	useEffect(() => {
+		const token = localStorage.getItem("JWT_USER_TOKEN");
+		axios.post('http://localhost:8080/users/isSigned', undefined, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			},
+			withCredentials: true
+		})
+			.then((res) => {
+				if (res.data.token) {
+					localStorage.setItem("JWT_USER_TOKEN", res.data.token);
+				}
+				setUserId(res.data.userId)
+			})
+	})
+
 	return (
-		<div className="font-mono h-[100%]">
-			<ToastContainer 
-				position="top-center"
-				hideProgressBar
-				toastClassName="bg-beige-100 text-black rounded-none ring-2 ring-black shadow-[4px_4px_black]"
-			/>
-			<RouterProvider router={router} />
-		</div>
+		<UserContext.Provider value={{ userId, setUserId }}>
+			<div className="font-mono">
+				<ToastContainer
+					position="top-center"
+					hideProgressBar
+					toastClassName="bg-beige-100 text-black rounded-none ring-2 ring-black shadow-[4px_4px_black]"
+				/>
+				<RouterProvider router={router} />
+			</div>
+		</UserContext.Provider>
 	)
 }
 
